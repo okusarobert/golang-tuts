a471bc3cb02269c1af91afe9f50a43fb14c6e6c6
Okusa John Robert <30295012+okusarobert@users.noreply.github.com>
2024-05-03 22:26:26 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+var someName = "hello"
+
+func main() {
+
+	// variables
+	// strings
+	// var nameOne string = "okusa"
+	// var nameTwo = "robert"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "Peach"
+	// nameThree = "bowser"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "Yoshi"
+
+	// fmt.Println(nameFour, someName, nameOne)
+
+	// ints
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits and memory
+	// var numOne int8 = 127
+	// var numTwo int8 = -128
+	// var numThree uint8 = 255
+
+	var scoreOne float32 = 6.8
+	var scoreTwo float64 = 3744894898399939.78
+
+	scoreThree := 37488488484.78
+
+}
