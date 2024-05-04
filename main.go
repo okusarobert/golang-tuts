8f00958b8f3a200927dd0d198e38a07e23835cc0
Okusa John Robert <30295012+okusarobert@users.noreply.github.com>
2024-05-04 12:05:33 +0300

    done

--- main.go
+++ main.go
@@ -2,43 +2,13 @@ package main
 
 import "fmt"
 
-var someName = "hello"
+var score = 99.5
 
 func main() {
 
-	// variables
-	// strings
-	// var nameOne string = "okusa"
-	// var nameTwo = "robert"
-	// var nameThree string
+	sayHello("Mario")
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
-
-	// nameOne = "Peach"
-	// nameThree = "bowser"
-
-	// fmt.Println(nameOne, nameTwo, nameThree)
-
-	// nameFour := "Yoshi"
-
-	// fmt.Println(nameFour, someName, nameOne)
-
-	// ints
-
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	// bits and memory
-	// var numOne int8 = 127
-	// var numTwo int8 = -128
-	// var numThree uint8 = 255
-
-	var scoreOne float32 = 6.8
-	var scoreTwo float64 = 3744894898399939.78
-
-	scoreThree := 37488488484.78
-
+	for _, v := range points {
+		fmt.Println(v)
+	}
 }
